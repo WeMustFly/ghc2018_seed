85117f80e8c85d2bdea1c199022247f7a19cb5df
Oleg Lobanov <terminataaar4@gmail.com>
2018-02-06 19:11:43 +0200

    Add app.js and run.js.

---
+++ app.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const fs = require('fs')
+const run = require('./run')
+
+const pathIn = './in'
+const pathOut = './out'
+const keys = ['R', 'C', 'L', 'H']
+const filenames = ['example.in', 'small.in', 'medium.in', 'big.in']
+
+fs.readdir(pathIn, (err, items) => {
+	if (err) {
+		return
+	} 
+
+	for (let item of items) {
+		if (filenames.indexOf(item) === -1) {
+			continue
+		}
+
+		let input = {}
+		let output = ''
+
+		input.data = []
+
+		const fileContent = fs.readFileSync(pathIn + '/' + item, 'ascii')
+
+		fileContent
+			.split("\n")
+			.map((line, i) => {
+				if (i === 0) {
+					let values = line.split(' ')
+					keys.map((key, i) => input[key] = +values[i])
+				} else {
+					if (line !== '') {
+					    input.data.push(line)
+					}
+				}
+		    })
+
+		let filenameOut = item.split('.')[0] + '.out'
+
+		output = run(input)
+
+		fs.writeFile(pathOut + "/" + filenameOut, output, err => {
+			if (err) {
+				return console.log(err)
+			}
+
+			console.log(`The file '${filenameOut}' was saved!`)
+		}) 
+	}
+})
---
+++ run.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const run = input => {
+	let output = ''
+
+	console.log(input)
+
+	return output
+}
+
+module.exports = run
