94cd9a8f4a3006a7f8defb93fa119e2eabdd1bc3
Oleg L <o@wemustfly.name>
2018-02-19 09:29:37 +0200

    Use JS Standart. Refactor Processors.

--- src/model.js
+++ src/model.js
@@ -1,26 +1,28 @@
 'use strict'
 
-const {Entity} = require('./entities')
-const {Processor} = require('./processors')
+const { Entity } = require('./entities')
+const { Processor } = require('./processors')
 
 class Model {
-	constructor(input) {
-		this.input = input
-		this.entities = {input: [], output: []}
-		this.parseInput()
-	}
+  constructor (input) {
+    this.input = input
+    this.entities = { input: [], output: [] }
+    this.parseInput()
+  }
 
-	parseInput() {
-		this.entities.input = this.input
-	}
+  parseInput () {
+    let entity = new Entity()
+    this.entities.input.push(entity)
+  }
 
-	run() {
-		this.entities.output = this.entities.input
-	}
+  process () {
+    let processor = new Processor(this.entities)
+    processor.process()
+  }
 
-	parseOutput() {
-		return this.entities.output.toString()
-	}
+  parseOutput () {
+    return this.entities.output.toString()
+  }
 }
 
 module.exports = Model
--- src/processors.js
+++ src/processors.js
@@ -1,6 +1,13 @@
 'use strict'
 
 class Processor {
+  constructor (entities) {
+    this.entities = entities
+  }
+
+  process () {
+    this.entities.output = this.entities.input
+  }
 }
 
 module.exports.Processor = Processor
