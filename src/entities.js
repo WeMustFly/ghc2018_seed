bf93bf44b2ee7ead02f5eb0f855d2b1e5dac61d6
Oleg L <o@wemustfly.name>
2018-03-01 20:12:55 +0200

    Rides, Vehicles.

--- src/entities.js
+++ src/entities.js
@@ -1,9 +1,20 @@
 'use strict'
 
-class Entity {
-  constructor (test) {
-    this.test = test
+class Vehicle {
+  constructor (id) {
+    this.id = id
+    this.currentPos = {r: 0, c: 0}
   }
 }
 
-module.exports.Entity = Entity
+class Ride {
+  constructor (id, a, b, x, y, s, f) {
+    this.id = id
+    this.begin = {a, b}
+    this.end = {x, y}
+    this.startStep = s
+    this.finishStep = f
+  }
+}
+
+module.exports = {Vehicle, Ride}
--- src/model.js
+++ src/model.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { Entity } = require('./entities')
+const { Vehicle, Ride } = require('./entities')
 const { Processor } = require('./processors')
 
 class Model {
@@ -11,8 +11,33 @@ class Model {
   }
 
   parseInput () {
-    let entity = new Entity()
-    this.entities.input.push(entity)
+    let from = this.input
+    let to = this.entities.input
+
+    to.R = from[0][0]
+    to.C = from[0][1]
+    to.F = from[0][2]
+    to.N = from[0][3]
+    to.B = from[0][4]
+    to.T = from[0][5]
+
+    to.vehicles = []
+    for (let i = 0; i < to.F; i++) {
+      to.vehicles.push(new Vehicle(i))
+    }
+
+    to.rides = []
+    for (let i = 0; i < to.N; i++) {
+      to.rides.push(new Ride(
+        i,
+        from[i + 1][0],
+        from[i + 1][1],
+        from[i + 1][2],
+        from[i + 1][3],
+        from[i + 1][4],
+        from[i + 1][5]
+      ))
+    }
   }
 
   process () {
